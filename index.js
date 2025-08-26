de3fdb79df64f6b280a68e5605613e95c594c52f
hillary <hillary.eduarda@estudante.ifro.edu.br>
2025-08-26 09:12:55 -0400

    lalala

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ import express from "express"
 
 const app  = express();
 
-app.get('/ufs'(req, res) =>{
+app.get('/loj',(req, res) =>{
     res.json(coleçãodeLoja);
 });
+
+app.get('/loj/:idloj', (req, res) => {
+    const idLOJ = parseInt(req.params.idloj);
+    const loj = coleçãodeLoja.find(loja => loja.id == idLOJ);
+    res.json(loj);
+})
+
+app.get('//loj/idloj', (req,res) => {
+    const idloj = parseInt(req.params.idloj);
+    let mensagemErro = "";
+    let loj;
+
+
+    if(!(isNaN(idLOJ))) {
+        loj = coleçãodeLoja.find(loja => loja.id === idLOJ);
+        if(!uf){
+            mensagemErro = "loja não encontrada"
+        }
+    }
+    }
+})
